4635e0b5402a2db555f14a9e97e2b231997343b8
ayushpal1810-svg <ayushpal1810@gmail.com>
2026-07-21 08:02:45 +0530

    completing dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -9,13 +9,53 @@ const Dashboard = () => {
     const [githuburl, setgithuburl] = useState("")
     const [linkedinurl, setlinkedinurl] = useState("")
     const [skills, setskills] = useState([])
+    const [skillstring, setskillstring] = useState("")
     const [projects, setProjects] = useState([]);
+    const [isprofileexist, setisprofileexist] = useState(true)
 
     const [currentProject, setCurrentProject] = useState({
         title: "",
         description: "",
         techstack: "",
     });
+    const fetchprofile=()=>{
+
+    }
+    const addproject = () => {
+        if (
+            !currentProject.title ||
+            !currentProject.description ||
+            !currentProject.techstack
+        ) {
+            alert("Please fill all project fields.");
+            return;
+        }
+        const project = {
+            ...currentProject,
+            techstack: currentProject.techstack.split(",").map((tech) => tech.trim()),
+        }
+        setProjects([...projects, project]);
+        setCurrentProject({
+            title: "",
+            description: "",
+            techstack: "",
+        });
+
+    }
+    const deleteProject = (index) => {
+        const updatedProjects = projects.filter((project, i) => {
+            if (i !== index) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+
+        setProjects(updatedProjects);
+    };
+    const saveprofile = ()=>{
+
+    }
     return (
         <div>
             <h2>Education/Degree</h2>
@@ -60,12 +100,38 @@ const Dashboard = () => {
                 onChange={(e) => setlinkedinurl(e.target.value)}
             />
             <h2>skills</h2>
-            <input type="text" />
+            <input
+                type="text"
+                placeholder='skills'
+                value={skillstring}
+                onChange={(e) => setskillstring(e.target.value)} />
+            <h2>projects</h2>
+            <input
+                type="text"
+                placeholder='title' value={currentProject.title}
+                onChange={(e) => setCurrentProject({ ...currentProject, title: e.target.value })} />
+            <input type="text"
+                placeholder='description' value={currentProject.description}
+                onChange={(e) => setCurrentProject({ ...currentProject, description: e.target.value })} />
+            <input type="text"
+                placeholder='techstack' value={currentProject.techstack}
+                onChange={(e) => setCurrentProject({ ...currentProject, techstack: e.target.value })} />
+            <button onClick={addproject}>add project</button>
+            <div>
+                {projects.map((project, index) => (
+                    <div style={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: "10px",
+                    }} key={index}>
+                        <h3>{project.title}</h3>
+                        <button onClick={() => deleteProject(index)}>X</button>
+                    </div>
+                ))} </div>
 
-
-
+            <button onClick={()=>saveprofile}>save</button>
         </div>
-        
+
     )
 }
 
